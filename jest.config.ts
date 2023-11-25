9964d86540988aac943ba55d072d1623f12a399d
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-11-25 19:51:13 -0300

    config: add the parse for css files

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
    verbose: true,
    testEnvironment: 'jest-environment-jsdom',
    setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+   moduleNameMapper: {
+      '\\.css$': '<rootDir>/emptyModule.js',
+    },
    collectCoverageFrom: [
       '<rootDir>/src/**/*.{js, ts, jsx, tsx}'
    ],
