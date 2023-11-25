39a452edd9eb5216b37840dd8d42ea83991b49b3
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-11-25 18:12:02 -0300

    test: created the new tests

--- src/components/loginForm/__test__/loginForm.spec.tsx
+++ src/components/loginForm/__test__/loginForm.spec.tsx
@@ -1,5 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import LoginForm from '..';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const axiosMock = new MockAdapter(axios);
 
 describe('<LoginForm />', () => {
   it('should render the form element', () => {
@@ -9,9 +13,9 @@ describe('<LoginForm />', () => {
   });
 
   it('should render the username input', () => {
-    const form = render(<LoginForm />);
+    render(<LoginForm />);
 
-    const usernameInput = form.container.querySelector('#username');
+    const usernameInput = screen.getByLabelText(/username/i);
     const label = screen.getByText(/username/i);
   
     expect(usernameInput).toBeInTheDocument();
@@ -19,9 +23,9 @@ describe('<LoginForm />', () => {
   });
 
   it('should render the password input', () => {
-    const form = render(<LoginForm />);
+    render(<LoginForm />);
 
-    const passowordInput = form.container.querySelector('#password');
+    const passowordInput = screen.getByLabelText(/password/i);
     const label = screen.getByText(/password/i);
   
     expect(passowordInput).toBeInTheDocument();
@@ -33,4 +37,39 @@ describe('<LoginForm />', () => {
     const submitInput = screen.getByText(/sign in/i);
     expect(submitInput).toBeInTheDocument();
   });
+
+  it('should render error message if any field is blank', async () => {
+    render(<LoginForm />);
+
+    const button = screen.getByText(/sign in/i);
+    fireEvent.click(button);
+
+    const elements = await screen.findAllByText(/Campo obrigatório/i);
+
+    // checks if exists two elements with the same error message (username and password)
+    expect(elements.length).toBeCloseTo(2);
+  })
+
+  it('should render error message if user not found', async () => {
+    // mock response
+    axiosMock.onPost().reply(401, 'unauthorized');
+
+    render(<LoginForm />);
+
+    const usernameInput = screen.getByLabelText(/username/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+  
+    // mock input data
+    fireEvent.change(usernameInput, {target: {value: 'test'}});
+    fireEvent.change(passwordInput, {target: {value: 'test'}});
+
+    const button = screen.getByText(/sign in/i);
+    fireEvent.click(button);
+
+    await screen.findByText(/usuário ou senha inválidos/i);
+  })
+
+  it('should access the admin page', () => {
+
+  })
 });
--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -1,17 +1,107 @@
+import { FormEvent, useState } from "react";
 import MainContainer from "../mainContainer";
+import axios from "axios";
 import './loginForm.css';
 
 export default function LoginForm(): JSX.Element {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameErrorMessage, setUsernameErrorMessage] = useState('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [refreshToken, setRefreshToken] = useState('');
+
+  async function handleForm(event: FormEvent) {
+    event.preventDefault();
+
+    setUsernameErrorMessage(username ? '' : 'Campo obrigatório');
+    setPasswordErrorMessage(password ? '' : 'Campo obrigatório');
+
+    if (!username || !password) return;
+
+    const url = 'http://127.0.0.1:8000/api/token/'
+   
+    axios({
+      method: 'POST',
+      url: url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        username: username,
+        password: password
+      }
+    })
+    .then(response => {
+      localStorage.setItem('tokenRefresh', JSON.stringify(response.data.refresh));
+      setRefreshToken(JSON.stringify(response.data.refresh));
+    })
+    .catch(() => {
+      setUsernameErrorMessage('Usuário ou senha inválidos');
+    })
+
+    if(refreshToken) {
+      axios({
+        method: 'POST',
+        url: 'http://127.0.0.1:8000/api/token/refresh/',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: {
+          refresh: JSON.parse(refreshToken)
+        }
+      })
+      .then(response => {
+        axios({
+          method: 'POST',
+          url: 'http://127.0.0.1:8000/api/token/verify/',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          data: {
+            token: response.data.access
+          }
+        })
+        .then(() => alert('logado com sucesso'))
+        .catch(() => alert('credenciais inválidas'))
+      });
+    }
+  }
+
   return (
     <MainContainer>
-      <form name='login_form' id='login_form' className="Login-form">
+      <form 
+        name='login_form' 
+        id='login_form' 
+        className="Login-form"
+        onSubmit={handleForm}
+        >
         <div>
           <label htmlFor="username" className="Login-form__label">Username</label>
-          <input type="text" name="username" id="username" className="Login-form__input"/>
+          <input 
+            type="text" 
+            name="username" 
+            id="username" 
+            className="Login-form__input"
+            value={username}
+            onChange={(e) => {setUsername(e.target.value)}}
+            />
+          {usernameErrorMessage && (
+            <p style={{ color: 'darkred' }}>{usernameErrorMessage}</p>
+          )}
         </div>
         <div>
           <label htmlFor="password" className="Login-form__label">Password</label>
-          <input type="password" name="password" id="password" className="Login-form__input"/>
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            className="Login-form__input"
+            value={password}
+            onChange={(e) => {setPassword(e.target.value)}}
+            />
+          {passwordErrorMessage && (
+            <p style={{ color: 'darkred' }}>{passwordErrorMessage}</p>
+          )}
         </div>
         <input type="submit" id="submit" value="sign in" className="Login-form__submit"/>
       </form>
