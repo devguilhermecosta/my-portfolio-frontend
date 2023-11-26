db11beeb7a1a1f4a8b61209a1ab284d27f7da3d6
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-11-26 18:14:25 -0300

    test: created the new tests

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,20 +1,52 @@
 import LoginForm from '../../components/loginForm';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 export default function Admin(): JSX.Element {
-  const [refreshToken, setRefreshToken] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [tokenRefresh] = useState(localStorage.getItem('tokenRefresh'));
+  const [tokenAccess, setTokenAccess] = useState('');
 
   useEffect(() => {
-    const token = localStorage.getItem('tokenRefresh');
-    if (token) setRefreshToken(token);
-  }, [refreshToken]);
+    if (!tokenRefresh) {
+      setIsAuthenticated(false);
+      return;
+    }
 
-  // método para verificar se o token refresh é válido
+    async function tokenRefreshVerify() {
+      await axios.post('http://127.0.0.1:8000/api/token/refresh/', {
+        refresh: JSON.parse(tokenRefresh ? tokenRefresh : '')
+      })
+      .then((response) => setTokenAccess(response.data.access))
+      .catch(() => setIsAuthenticated(false))
+    }
+
+    tokenRefreshVerify();
+
+  }, [tokenRefresh]);
+
+  useEffect(() => {
+    async function tokenAccessVerify(): Promise<void> {
+      await axios.post('http://127.0.0.1:8000/api/token/verify/', {
+        token: tokenAccess
+      })
+      .then(() => setIsAuthenticated(true))
+      .catch(() => setIsAuthenticated(false))
+    }
+
+    tokenAccessVerify();
+
+  }, [tokenAccess])
 
 
-  if (!refreshToken) return <LoginForm />;
+  if (isAuthenticated) {
+    return <Navigate to={'/admin/dashboard'}/>
+  }
 
+  // if the token is not valid or
+  // token does not exists, render the login form
   return (
-    <section></section>
+    <LoginForm />
   )
 }
