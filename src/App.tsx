48720f109e9d6175a4342e341dcbb5ea5f936782
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-11-26 18:13:45 -0300

    update: add the router to Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Admin from './pages/admin';
+import Dashboard from './pages/dashboard';
 
 export const router = createBrowserRouter([
   {
     path: '/admin',
     element: <Admin />
   },
+  {
+    path: '/admin/dashboard',
+    element: <Dashboard />
+  }
 ])
