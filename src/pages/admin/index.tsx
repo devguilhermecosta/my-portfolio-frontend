5a2a40231f70db41e192ae859a8f8de9c2350104
Guilherme Costa <guilherme.partic@gmail.com>
2023-11-29 15:15:32 -0300

    update: add the logout button

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,17 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/authContext';
+
 export default function Dashboard(): JSX.Element {
+  const { handleLogout } = useContext(AuthContext);
+
   return (
     <section id="dashboard">
       <h1>administration area</h1>
+      <button
+        onClick={handleLogout}
+      >
+        logout
+      </button>
     </section>
   )
 }
