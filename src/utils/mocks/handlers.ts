fe2c50520f7cd85af7210a5639f8226531c3c8ea
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-02 18:38:33 -0300

    update: add handler to /api/networks/v1/

--- src/utils/mocks/handlers.ts
+++ src/utils/mocks/handlers.ts
@@ -10,4 +10,14 @@ export const handlers = [
   http.post('http://127.0.0.1:8000/api/token/verify/', () => {
     return new HttpResponse('any');
   }),
+  http.get('http://127.0.0.1:8000/networks/api/v1/', () => {
+    return HttpResponse.json({
+        email: "guilherme@email.com",
+        github: "https://github.com",
+        instagram: "https://instagram.com",
+        linkedin: "https://linkedin.com",
+        phone: "46999083251",
+        whatsapp: "https://whatsapp.com",
+    }, { status: 200 });
+  })
 ]
