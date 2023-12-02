2adeb33020364253519256e1fa1bd3e9d452dbc2
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-02 15:17:21 -0300

    conf: created the new handlers

--- src/setup.ts
+++ src/setup.ts
@@ -1,14 +1,6 @@
 import { beforeAll, afterAll, afterEach } from 'vitest';
 import { server } from "./utils/mocks/node";
 
-beforeAll(() => {
-  server.listen();
-
-  console.log('LISTENING...');
-  
-  server.events.on('request:start', ({ request }) => {
-    console.log('MSW intercepted:', request.method, request.url)
-  })
-});
+beforeAll(() => server.listen());
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
