a2ad701e7bf6764e26ac9b96e42d8d04c8af6f29
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-02 17:19:25 -0300

    test: created the new tests

---
+++ src/pages/admin/__tests__/admin.spec.tsx
@@ -0,0 +1,67 @@
+import '@testing-library/jest-dom';
+import { describe, it, expect } from 'vitest';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from '../../../contexts/authContext';
+import Dashboard from '..';
+
+const renderDashboard = () => {
+  return render(
+    <AuthProvider>
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+describe('<Dashboard />', () => {
+  it('should render the correct content', async () => {
+    renderDashboard();
+
+    await screen.findByText(/administration area/i);
+    await screen.findByText(/logout/i);
+    await screen.findByText(/networks/i);
+    await screen.findByText(/^works$/i);
+
+  });
+
+  it('should delete the user token on press the logout button', async () => {
+    renderDashboard();
+
+    const logoutButton = screen.getByText(/logout/i);
+
+    fireEvent.click(logoutButton);
+
+    /**
+     * the user must be redirected to login page
+     */
+    await waitFor(() => expect(window.location.href).toContain('/admin/login'));
+
+    /**
+     * this means the user token has been removed, 
+     * otherwise the user would be redirected to /admin/dashboard
+     */
+    await waitFor(() => expect(window.location.href).not.toContain('/admin/dashboard'));
+  });
+
+  it('should navigate the user to networks page', async () => {
+    renderDashboard();
+
+    const logoutButton = screen.getByText(/^networks$/i);
+
+    fireEvent.click(logoutButton);
+
+    await waitFor(() => expect(window.location.href).toContain('/admin/dashboard/networks'));
+  });
+
+  it('should navigate the user to works page', async () => {
+    renderDashboard();
+
+    const logoutButton = screen.getByText(/^works$/i);
+
+    fireEvent.click(logoutButton);
+
+    await waitFor(() => expect(window.location.href).toContain('/admin/dashboard/works'));
+  });
+})
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,17 +1,44 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/authContext';
+import MainContainer from '../../components/mainContainer';
+import Loading from '../../components/loading';
+import Style from './admin.module.css';
 
 export default function Dashboard(): JSX.Element {
   const { handleLogout } = useContext(AuthContext);
+  const [loading, setloading] = useState(false);
+  const navigate = useNavigate();
 
   return (
-    <section id="dashboard">
-      <h1>administration area</h1>
-      <button
-        onClick={handleLogout}
-      >
-        logout
-      </button>
-    </section>
+    <MainContainer justifyContent='flex-start' flexDirection='column'>
+      {loading && (
+        <Loading />
+      )}
+
+      <section className={Style.C_dashboard}>
+        <h1>administration area</h1>
+        <button onClick={() => {
+          setloading(true);
+          handleLogout();
+          navigate('/admin/login');
+        }}
+          className={Style.C_dashboard_button_logout}>
+          logout
+        </button>
+        <button 
+          className={Style.C_dashboard_button}
+          onClick={() => navigate('/admin/dashboard/networks')}
+        >
+          networks
+        </button>
+        <button 
+          className={Style.C_dashboard_button}
+          onClick={() => navigate('/admin/dashboard/works')}
+        >
+          works
+        </button>
+      </section>
+    </MainContainer>
   )
 }
