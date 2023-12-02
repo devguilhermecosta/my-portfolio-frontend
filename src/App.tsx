a1284e039f35616e961036aa1b49e7c37e418ee0
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-02 17:18:55 -0300

    feat: add the routes to Works and Networks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Dashboard from './pages/admin';
 import LoginPage from './pages/login';
+import Networks from './pages/networks';
+import Works from './pages/works';
 import PrivateRoute from './components/privateRoute';
 
 export const router = createBrowserRouter([
@@ -12,4 +14,12 @@ export const router = createBrowserRouter([
     path: 'admin/dashboard',
     element: <PrivateRoute><Dashboard /></PrivateRoute>
   },
+  {
+    path: 'admin/dashboard/networks',
+    element: <PrivateRoute><Networks /></PrivateRoute>
+  },
+  {
+    path: 'admin/dashboard/works',
+    element: <PrivateRoute><Works /></PrivateRoute>
+  }
 ])
