ec872d5628a6dca826f5914a55ac637b1337d9ee
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-03 18:05:44 -0300

    update: created the function to update the networks

--- src/pages/networks/__tests__/networks.spec.tsx
+++ src/pages/networks/__tests__/networks.spec.tsx
@@ -1,18 +1,72 @@
 import '@testing-library/jest-dom';
-import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, it, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
 import Networks from '..';
+import userEvent from '@testing-library/user-event';
+import { server } from '../../../utils/mocks/node';
+import { http, HttpResponse } from 'msw';
+import { BrowserRouter } from 'react-router-dom';
+
+const renderNetworks = () => {
+  return render(
+    <BrowserRouter>
+      <Networks />
+    </BrowserRouter>
+  );
+}
 
 describe('<Networks />', () => {
   it('should render the networks', async () => {
-    render(<Networks />);
+    renderNetworks();
   
-    await screen.findByText('guilherme@email.com');
-    await screen.findByText('https://github.com');
-    await screen.findByText('https://instagram.com');
-    await screen.findByText('https://linkedin.com');
-    await screen.findByText('46999083251');
-    await screen.findByText('https://whatsapp.com');
+    await screen.findByDisplayValue('guilherme@email.com');
+    await screen.findByDisplayValue('https://github.com');
+    await screen.findByDisplayValue('https://instagram.com');
+    await screen.findByDisplayValue('https://linkedin.com');
+    await screen.findByDisplayValue('46999083251');
+    await screen.findByDisplayValue('https://whatsapp.com');
+    await screen.findByText('save');
 
-  })
+  });
+
+  it('should return error message', async () => {
+    server.use(
+      http.patch('http://127.0.0.1:8000/networks/api/v1/', () => {
+        return new HttpResponse(null, { status: 400 })
+      })
+    );
+
+    renderNetworks();
+
+    const spy = vi.spyOn(console, 'error');
+    const submit = screen.getByText(/save/i);
+    await userEvent.click(submit);
+
+    expect(spy).toHaveBeenCalledWith('error on save: AxiosError: Request failed with status code 400');
+  });
+
+  it('should return success message', async () => {
+    server.use(
+      http.patch('http://127.0.0.1:8000/networks/api/v1/', () => {
+        return new HttpResponse(null, { status: 204 })
+      })
+    );
+
+    renderNetworks();
+
+    const spy = vi.spyOn(console, 'log');
+    const submit = screen.getByText(/save/i);
+    await userEvent.click(submit);
+
+    expect(spy).toHaveBeenCalledWith('save successfully with status code 204');
+  });
+
+  it('should return to dashboard', async () => {
+    renderNetworks();
+
+    const backButton = screen.getByText(/</i);
+    await userEvent.click(backButton);
+
+    await waitFor(() => expect(window.location.href).toContain('/admin/dashboard'));
+  });
 })
--- src/pages/networks/index.tsx
+++ src/pages/networks/index.tsx
@@ -1,92 +1,116 @@
 import MainContainer from "../../components/mainContainer";
-import { useState, useEffect } from 'react';
-import Style from './networks.module.css';
+import { useState, useEffect, FormEvent, useContext } from 'react';
 import axios from "axios";
-
-interface NetworksProps {
-  instagram?: string;
-	linkedin?: string;
-	github?: string;
-	whatsapp?: string;
-	phone?: string;
-	email?: string;
-}
+import Input from "../../components/input";
+import { AuthContext } from "../../contexts/authContext";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import BackButton from "../../components/backButton";
 
 export default function Networks(): JSX.Element {
-  const [networkList, setNetworkList] = useState<NetworksProps>();
   const [instagram, setInstagram] = useState<string | undefined>('');
   const [linkedin, setLinkedin] = useState<string | undefined>('');
   const [github, setGithub] = useState<string | undefined>('');
   const [whatsapp, setWhatsapp] = useState<string | undefined>('');
   const [phone, setPhone] = useState<string | undefined>('');
   const [email, setEmail] = useState<string | undefined>('');
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function getNetworks() {
       await axios.get('http://127.0.0.1:8000/networks/api/v1/')
-      .then(response => setNetworkList(response.data))
-      .catch((e) => console.log('error: ', e))
+      .then(response => {
+        setInstagram(response.data.instagram);
+        setLinkedin(response.data.linkedin);
+        setGithub(response.data.github);
+        setWhatsapp(response.data.whatsapp);
+        setPhone(response.data.phone);
+        setEmail(response.data.email);
+      })
     }
 
     getNetworks();
   
   }, []);
 
-  useEffect(() => {
-    if (networkList) {
-      setInstagram(networkList?.instagram);
-      setLinkedin(networkList?.linkedin);
-      setGithub(networkList?.github);
-      setWhatsapp(networkList?.whatsapp);
-      setPhone(networkList?.phone);
-      setEmail(networkList?.email);
+
+  async function handleUpdate(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+ 
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${user}`,
+      },
     }
-  }, [networkList])
+
+    const data = {
+      instagram: formData.get('instagram'),
+      linkedin: formData.get('linkedin'),
+      github: formData.get('github'),
+      whatsapp: formData.get('whatsapp'),
+      phone: formData.get('phone'),
+      email: formData.get('email')
+    }
+
+    await axios.patch('http://127.0.0.1:8000/networks/api/v1/', data, config)
+    .then((response) => {
+      toast.success('save successfully');
+      console.log(`save successfully with status code ${response.status}`);
+    })
+    .catch((e) => {
+      toast.error('error on save');
+      console.error(`error on save: ${e}`);
+    })
+  }
 
   return (
     <MainContainer>
+      <BackButton onClick={() => navigate('/admin/dashboard')}/>
       <h1>Networks</h1>
-      {networkList && (
-        <form action="">
-          className={Style.input}
-          <input 
-            type="text" 
-            value={instagram}
-            onChange={(e) => {setInstagram(e.target.value)}}
-            className={Style.input}
-          />
-          <input 
-            type="text" 
-            value={linkedin}
-            onChange={(e) => {setLinkedin(e.target.value)}}
-            className={Style.input}
-          />
-          <input 
-            type="text" 
-            value={github}
-            onChange={(e) => {setGithub(e.target.value)}}
-            className={Style.input}
-          />
-          <input 
-            type="text" 
-            value={whatsapp}
-            onChange={(e) => {setWhatsapp(e.target.value)}}
-            className={Style.input}
-          />
-          <input 
-            type="text" 
-            value={phone}
-            onChange={(e) => {setPhone(e.target.value)}}
-            className={Style.input}
-          />
-          <input 
-            type="text" 
-            value={email}
-            onChange={(e) => {setEmail(e.target.value)}}
-            className={Style.inpinputElementut}
-          />
-        </form>
-      )}
+      <form action="" onSubmit={handleUpdate}>
+        <Input labelName="instagram"
+          value={instagram}
+          onChange={(e) => {setInstagram(e.target.value)}}
+        />
+        <Input labelName="linkedin"
+          value={linkedin}
+          onChange={(e) => {setLinkedin(e.target.value)}}
+        />
+        <Input labelName="github"
+          value={github}
+          onChange={(e) => {setGithub(e.target.value)}}
+        />
+        <Input labelName="whatsapp"
+          value={whatsapp}
+          onChange={(e) => {setWhatsapp(e.target.value)}}
+        />
+        <Input labelName="phone"
+          value={phone}
+          onChange={(e) => {setPhone(e.target.value)}}
+        />
+        <Input labelName="email"
+          value={email}
+          onChange={(e) => {setEmail(e.target.value)}}
+        />
+        <input 
+          type="submit" 
+          value="save"
+          style={{
+            marginTop: '15px',
+            width: '100%',
+            padding: '8px',
+            borderRadius: '4px',
+            border: 'none',
+            outline: 'none',
+            backgroundColor: 'var(--contrast-std)',
+            color: 'var(--secondaire-l1)',
+            cursor: 'pointer',
+          }}/>
+      </form>
     </MainContainer>
   )
 }
