a08199debfde053b7c59a25ea8e5fd1e70f5ba2c
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-03 18:04:14 -0300

    update: add the Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './App';
 import { AuthProvider } from './contexts/authContext';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
+      <Toaster position='top-right'/>
       <RouterProvider router={router}/>
     </AuthProvider>
   </React.StrictMode>,
