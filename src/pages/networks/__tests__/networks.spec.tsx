0e66c1ba837b363682e154737179583a245b72e7
Guilherme Costa <guilherme.partic@gmail.com>
2023-12-05 18:11:59 -0300

    test: created the tests to post request

--- src/pages/networks/__tests__/networks.spec.tsx
+++ src/pages/networks/__tests__/networks.spec.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { describe, it, vi } from 'vitest';
+import { describe, it, vi, expect } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
 import Networks from '..';
 import userEvent from '@testing-library/user-event';
@@ -29,7 +29,7 @@ describe('<Networks />', () => {
 
   });
 
-  it('should return error message', async () => {
+  it('should return error message when patch request', async () => {
     server.use(
       http.patch('http://127.0.0.1:8000/networks/api/v1/', () => {
         return new HttpResponse(null, { status: 400 })
@@ -45,7 +45,7 @@ describe('<Networks />', () => {
     expect(spy).toHaveBeenCalledWith('error on save: AxiosError: Request failed with status code 400');
   });
 
-  it('should return success message', async () => {
+  it('should return success message when patch request', async () => {
     server.use(
       http.patch('http://127.0.0.1:8000/networks/api/v1/', () => {
         return new HttpResponse(null, { status: 204 })
@@ -68,5 +68,44 @@ describe('<Networks />', () => {
     await userEvent.click(backButton);
 
     await waitFor(() => expect(window.location.href).toContain('/admin/dashboard'));
+  });
+
+  it('should render red input fields when error on post request', async () => {
+    server.use(
+      http.get('http://127.0.0.1:8000/networks/api/v1/', () => {
+        return new HttpResponse(null, { status: 404 })
+      }),
+      http.post('http://127.0.0.1:8000/networks/api/v1/', () => {
+        return HttpResponse.json({
+          email: 'Campo obrigatório',
+          github: 'Campo obrigatório',
+          linkedin: 'Campo obrigatório',
+          phone: 'Campo obrigatório',
+          whatsapp: 'Campo obrigatório',
+          instagram: 'Campo obrigatório',
+        }, { status: 400 })
+      })
+    );
+
+    renderNetworks();
+
+    const submitButton = screen.getByText(/save/i);
+
+    await userEvent.click(submitButton);
+
+    const email = await screen.findByLabelText(/email/i);
+    const instagram = await screen.findByLabelText(/instagram/i);
+    const github = await screen.findByLabelText(/github/i);
+    const linkedin = await screen.findByLabelText(/linkedin/i);
+    const phone = await screen.findByLabelText(/phone/i);
+    const whatsapp = await screen.findByLabelText(/whatsapp/i);
+
+    expect(email).toHaveStyle({ border: '2px solid red' });
+    expect(instagram).toHaveStyle({ border: '2px solid red' });
+    expect(github).toHaveStyle({ border: '2px solid red' });
+    expect(linkedin).toHaveStyle({ border: '2px solid red' });
+    expect(phone).toHaveStyle({ border: '2px solid red' });
+    expect(whatsapp).toHaveStyle({ border: '2px solid red' });
+
   });
 })
--- src/pages/networks/index.tsx
+++ src/pages/networks/index.tsx
@@ -15,7 +15,14 @@ export default function Networks(): JSX.Element {
   const [whatsapp, setWhatsapp] = useState<string>('');
   const [phone, setPhone] = useState<string>('');
   const [email, setEmail] = useState<string>('');
+  const [instagramError, setInstagramError] = useState<string | undefined>();
+  const [linkedinError, setLinkedinError] = useState<string | undefined>();
+  const [githubError, setGithubError] = useState<string | undefined>();
+  const [whatsappError, setWhatsappError] = useState<string | undefined>();
+  const [phoneError, setPhoneError] = useState<string | undefined>();
+  const [emailError, setEmailError] = useState<string | undefined>();
   const { user } = useContext(AuthContext);
+  const [networkExists, setNetworkExists] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -28,6 +35,11 @@ export default function Networks(): JSX.Element {
         setWhatsapp(response.data.whatsapp);
         setPhone(response.data.phone);
         setEmail(response.data.email);
+        setNetworkExists(true);
+      })
+      .catch(() => {
+        toast.error('networks not found');
+        setNetworkExists(false);
       })
     }
 
@@ -57,15 +69,35 @@ export default function Networks(): JSX.Element {
       email: formData.get('email')
     }
 
-    await api.patch('/networks/api/v1/', data, config)
+    if (networkExists) {
+      await api.patch('/networks/api/v1/', data, config)
+      .then((response) => {
+        toast.success('save successfully');
+        console.log(`save successfully with status code ${response.status}`);
+      })
+      .catch((e) => {
+        toast.error('error on save');
+        console.error(`error on save: ${e}`);
+      })
+
+      return
+    }
+
+    await api.post('/networks/api/v1/', data, config)
     .then((response) => {
-      toast.success('save successfully');
-      console.log(`save successfully with status code ${response.status}`);
+      toast.success('created successfully');
+      console.log(`created successfully with status code ${response.status}`);
     })
     .catch((e) => {
       toast.error('error on save');
-      console.error(`error on save: ${e}`);
+      setInstagramError(e.response.data.instagram);
+      setLinkedinError(e.response.data.linkedin);
+      setGithubError(e.response.data.github);
+      setWhatsappError(e.response.data.whatsapp);
+      setPhoneError(e.response.data.phone);
+      setEmailError(e.response.data.email);
     })
+
   }
 
   return (
@@ -76,26 +108,32 @@ export default function Networks(): JSX.Element {
         <Input labelName="instagram"
           value={instagram}
           onChange={(e) => {setInstagram(e.target.value)}}
+          error={instagramError}
         />
         <Input labelName="linkedin"
           value={linkedin}
           onChange={(e) => {setLinkedin(e.target.value)}}
+          error={linkedinError}
         />
         <Input labelName="github"
           value={github}
           onChange={(e) => {setGithub(e.target.value)}}
+          error={githubError}
         />
         <Input labelName="whatsapp"
           value={whatsapp}
           onChange={(e) => {setWhatsapp(e.target.value)}}
+          error={whatsappError}
         />
         <Input labelName="phone"
           value={phone}
           onChange={(e) => {setPhone(e.target.value)}}
+          error={phoneError}
         />
         <Input labelName="email"
           value={email}
           onChange={(e) => {setEmail(e.target.value)}}
+          error={emailError}
         />
 
         <SubmitInput />
