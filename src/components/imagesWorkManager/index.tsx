095f86c6e7f1fd86290fee914050daecfe0eaf71
Guilherme Costa <guilherme.partic@gmail.com>
2023-12-06 18:06:11 -0300

    style: add animation to close the windows

--- src/components/imagesWorkManager/index.tsx
+++ src/components/imagesWorkManager/index.tsx
@@ -3,6 +3,7 @@ import SubmitInput from '../submitInput';
 import UploadInput from '../uploadInput';
 import { ChangeEvent, useState } from 'react';
 import { MdDelete } from "react-icons/md";
+import { IoCloseCircleSharp } from "react-icons/io5";
 
 interface ImageProps {
   image: File;
@@ -11,6 +12,7 @@ interface ImageProps {
 
 export default function ImagesWorkManager({ workId }: { workId: number }): JSX.Element {
   const [images, setImages] = useState<ImageProps[]>([]);
+  const [visible, setVisible] = useState(true);
 
   function handleImages(e: ChangeEvent<HTMLInputElement>) {
     const receivedImages = e.target.files;
@@ -35,28 +37,39 @@ export default function ImagesWorkManager({ workId }: { workId: number }): JSX.E
   }
 
   return (
-    <section className={Style.C_work_images_manager} >
-      <h1>now, add some images</h1>
+      <section className={`${Style.C_work_images_manager} ${!visible ? Style.close_windows : ''}`}>
 
-      <UploadInput multiple={true} onChange={(e) => handleImages(e)}/>
+        <IoCloseCircleSharp 
+          size={28} 
+          style={{ color: 'red', position: 'absolute', top: 0, right: 0}} 
+          onClick={() => setVisible(false)}
+        />
 
-      <section className={Style.C_work_images_grid}>
-        {images.length > 0 && images.map((image, index) => (
-          <div style={{ position: 'relative' }} key={index}>
-            <button className={Style.C_work_button_delete} onClick={() => handleDelete(image.url)}>
+        <h1>now, add some images</h1>
+
+        <UploadInput multiple={true} onChange={(e) => handleImages(e)}/>
+
+        <section className={Style.C_work_images_grid}>
+          {images.length > 0 && images.map((image, index) => (
+            <div style={{ position: 'relative' }} key={index}>
               <MdDelete 
                 size={28} 
-                style={{ color: 'var(--contrast-l1)', outline: 'none', cursor: 'pointer',}}
+                style={{ 
+                  color: 'var(--contrast-l1)', 
+                  cursor: 'pointer', 
+                  position: 'absolute', 
+                  top: 5, right: 5
+                }}
+                onClick={() => handleDelete(image.url)}
               />
-            </button>
-            <img src={image.url} alt="work image" className={Style.C_work_images_img}/>
-          </div>
-        ))}
-      </section>
+              <img src={image.url} alt="work image" className={Style.C_work_images_img}/>
+            </div>
+          ))}
+        </section>
 
-      {images.length !== 0 && (
-        <SubmitInput />
-      )}
-    </section>
+        {images.length !== 0 && (
+          <SubmitInput />
+        )}
+      </section>
   )
 }
