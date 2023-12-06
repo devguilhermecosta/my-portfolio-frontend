3dd0dbc543fa23e78ed62145b63b90ac2061704d
Guilherme Costa <guilherme.partic@gmail.com>
2023-12-06 16:05:17 -0300

    test: created a new test

--- src/pages/works/__tests__/works.spec.tsx
+++ src/pages/works/__tests__/works.spec.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { describe, it, expect } from 'vitest';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Works from '..';
 import { server } from '../../../utils/mocks/node';
 import { http, HttpResponse} from 'msw';
@@ -31,29 +31,29 @@ describe('<Works />', () => {
   it('should render the works', async () => {
     renderWorks();
 
+    /* see the handlers on ./src/utils/mocks/handlers.ts */
+
     await screen.findByText(/work title 1/i);
     await screen.findByText(/work title 2/i);
     await screen.findByText(/work title 3/i);
 
   });
 
-  it('should navigate to new work page', async () => {
+  it('should navigate to new work page', () => {
     renderWorks();
 
     const newWork = screen.getByText(/new work/i);
-
     fireEvent.click(newWork);
-
-    await waitFor(() => expect(window.location.href).toContain('/admin/dashboard/works/new'));
+    expect(window.location.href).toContain('/admin/dashboard/works/new');
+  
   });
 
-  it('should return to dashboard', async () => {
+  it('should return to dashboard', () => {
     renderWorks();
 
     const backButton = screen.getByText(/</i);
-
     fireEvent.click(backButton);
+    expect(window.location.href).toContain('/admin/dashboard');
 
-    await waitFor(() => expect(window.location.href).toContain('/admin/dashboard'));
   });
 })
