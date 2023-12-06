6a9b1a0f144f036463822ebad83bbe79279a5a0e
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-06 00:15:26 -0300

    update: add the new handler

--- src/utils/mocks/handlers.ts
+++ src/utils/mocks/handlers.ts
@@ -12,23 +12,16 @@ export const handlers = [
   http.post(`${baseUrl}/api/token/verify/`, () => {
     return new HttpResponse('any');
   }),
+  http.patch(`${baseUrl}/networks/api/v1/`, () => {
+    return new HttpResponse('any');
+  }),
   http.post(`${baseUrl}/networks/api/v1/`, () => {
     return new HttpResponse(null, { status: 201 })
   }),
   http.get(`${baseUrl}/networks/api/v1/`, () => {
-    return HttpResponse.json({
-        email: "guilherme@email.com",
-        github: "https://github.com",
-        instagram: "https://instagram.com",
-        linkedin: "https://linkedin.com",
-        phone: "46999083251",
-        whatsapp: "https://whatsapp.com",
-    }, { status: 200 });
+    return new HttpResponse(null, { status: 201 })
   }),
   http.post(`${baseUrl}/networks/api/v1/`, () => {
-    return new HttpResponse('any');
-  }),
-  http.patch(`${baseUrl}/networks/api/v1/`, () => {
     return new HttpResponse('any');
   }),
   http.get(`${baseUrl}/work/api/list/`, () => {
