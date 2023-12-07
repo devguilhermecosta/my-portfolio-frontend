08fe8cfe8fc1c2e3d0f2156282447452d4b0952e
Guilherme Costa <guilherme.partic@gmail.com>
2023-12-07 17:34:40 -0300

    test: created a new test

--- src/pages/works/new/__tests__/newWork.spec.tsx
+++ src/pages/works/new/__tests__/newWork.spec.tsx
@@ -7,16 +7,21 @@ import userEvent from '@testing-library/user-event';
 import { server } from '../../../../utils/mocks/node';
 import { http, HttpResponse } from 'msw';
 import { baseUrl } from '../../../../utils/api';
+import { act } from 'react-dom/test-utils';
 
 global.URL.createObjectURL = vi.fn();
 
+const handerNewWorkForm = () => {
+  return render(
+    <BrowserRouter>
+      <NewWork />
+    </BrowserRouter>
+  );
+}
+
 describe('<NewWork >', () => {
   it('should render a new work form', async () => {
-    render(
-      <BrowserRouter>
-        <NewWork />
-      </BrowserRouter>
-    );
+    handerNewWorkForm();
 
     await screen.findByText('<');
     await screen.findByLabelText(/title/i);
@@ -38,11 +43,7 @@ describe('<NewWork >', () => {
 
     const user = userEvent.setup();
 
-    const page = render(
-      <BrowserRouter>
-        <NewWork />
-      </BrowserRouter>
-    );
+    const page = handerNewWorkForm();
 
     const borderStyle = '2px solid red';
     const submitButton = screen.getByText(/save/i);
@@ -51,6 +52,29 @@ describe('<NewWork >', () => {
     expect(await screen.findByLabelText(/title/i)).toHaveStyle({ border: borderStyle });
     expect(await screen.findByLabelText(/description/i)).toHaveStyle({ border: borderStyle });
     expect(page.container.querySelector('#strokeCover')).toHaveStyle({ border: borderStyle });
+
+  });
+
+  it('should not load the work image manager if an error occurs in the work creation request', async () => {
+    server.use(http.post(`${baseUrl}/work/api/create/`, async () => {
+      return HttpResponse.json({
+        title: 'Campo obrigatório',
+        link: 'Campo obrigatório',
+        description: 'Campo obrigatório',
+        cover: 'Campo obrigatório',
+      }, { status: 400 })
+    }))
+
+    const user = userEvent.setup();
+
+    handerNewWorkForm();
+
+    const submitButton = screen.getByText(/save/i);
+    await user.click(submitButton);
+
+    const imagesManager = screen.queryByText(/now, add some images/i);
+
+    expect(imagesManager).not.toBeInTheDocument();
 
   });
 
@@ -66,11 +90,7 @@ describe('<NewWork >', () => {
 
     const user = userEvent.setup();
 
-    render(
-      <BrowserRouter>
-        <NewWork />
-      </BrowserRouter>
-    );
+    handerNewWorkForm();
 
     const submitButton = screen.getByText(/save/i);
     await user.click(submitButton);
@@ -79,7 +99,15 @@ describe('<NewWork >', () => {
 
   });
 
-  it('should clean the work form if the work is created', async () => {
+  it('should clean the work form if the work images are created', async () => {
+    /* work data to mock all inputs */
+    const workData = {
+      title: 'work title',
+      link: 'https://link.com',
+      description: 'work description',
+    };
+
+    /* mock the requests */
     server.use(
       http.post(`${baseUrl}/work/api/create/`, async () => {
       return HttpResponse.json({
@@ -95,22 +123,57 @@ describe('<NewWork >', () => {
     
     )
 
+    /* call userEvent */
     const user = userEvent.setup();
 
-    render(
-      <BrowserRouter>
-        <NewWork />
-      </BrowserRouter>
-    );
+    /* render the new work form */
+    handerNewWorkForm();
 
+    /**
+     * mocks the input data.
+     * this is not necessary but I want to test the inputs before form submission
+     */
+    const titleInput = screen.getByLabelText(/title/i);
+    const linkUnput = screen.getByLabelText(/link/i);
+    const descriptionInput = screen.getByLabelText(/description/i);
+    await user.type(titleInput, workData.title);
+    await user.type(linkUnput, workData.link);
+    await user.type(descriptionInput, workData.description);
+
+    expect(titleInput).toHaveDisplayValue(workData.title);
+    expect(linkUnput).toHaveDisplayValue(workData.link);
+    expect(descriptionInput).toHaveDisplayValue(workData.description);
+
+    /**
+     * mocks sending the form to register the work.
+     * after the work is saved, the form to upload all images is open 
+     */
     const submitButton = screen.getByText(/save/i);
     await user.click(submitButton);
 
+    /**
+     * mocks image upload
+     */
     const inputUpload = await screen.findByTestId(/upload_images/i);
+    const images = [
+      new File(['hello'], 'hello.png', {type: 'image/png'}),
+      new File(['there'], 'there.png', {type: 'image/png'}),
+    ];
+    user.upload(inputUpload, images)
 
-    const image = new File(['(⌐□_□)'], 'test.png', { type: 'images/png' })
-    await user.upload(inputUpload, image)
+    /* this button only appears when images are uploaded */
+    const buttonUpload = await screen.findByDisplayValue(/upload all images/i);
 
-    expect(await screen.findByDisplayValue(/upload all images/i))
+    /* click on button */
+    await act( () => userEvent.click(buttonUpload))
+    
+    /**
+     * when the work images are created;
+     * all inputs data must be cleaned.
+    */
+    expect(await screen.findByLabelText(/title/i)).toHaveDisplayValue('');
+    expect(await screen.findByLabelText(/link/i)).toHaveDisplayValue('');
+    expect(await screen.findByLabelText(/description/i)).toHaveDisplayValue('');
+
   });
 })
