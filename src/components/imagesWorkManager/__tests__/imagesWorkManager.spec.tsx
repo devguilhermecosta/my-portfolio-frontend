8c9e39790c17dd7a7f91c16cbfbd6fe393453ebe
Guilherme Costa <guilherme.partic@gmail.com>
2023-12-07 18:16:22 -0300

    test: created the new tests

--- src/components/imagesWorkManager/__tests__/imagesWorkManager.spec.tsx
+++ src/components/imagesWorkManager/__tests__/imagesWorkManager.spec.tsx
@@ -1,1 +1,32 @@
 // TODO create all tests
+import '@testing-library/jest-dom';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import ImagesWorkManager from '..';
+
+const handerImagesWorkManager = (workId: number, user: string | null) => {
+  return render(
+    <ImagesWorkManager workId={workId} user={user}/>
+  )
+}
+
+describe('<ImagesWorkManager />', () => {
+  it('should have the correct content', () => {
+    handerImagesWorkManager(1, 'user');
+
+    expect(screen.getByText(/now, add some images/)).toBeInTheDocument(); // title
+    expect(screen.getByTestId(/upload_images/)).toBeInTheDocument(); // input:file
+    expect(screen.getByTestId(/close_button/)).toBeInTheDocument(); // close button
+  });
+
+  it('should not render the upload button if at least one image is uploaded', () => {
+    handerImagesWorkManager(1, 'user');
+    const button = screen.queryByDisplayValue(/upload all images/i);
+    expect(button).not.toBeInTheDocument();
+  });
+
+  it('should render upload button only when some image is uploaded', () => {
+
+
+  });
+})
