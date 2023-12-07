4fcdc769823881d01f8cc0b60dcec0620755622e
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-07 00:26:32 -0300

    update: add a test id property

--- src/components/submitInput/index.tsx
+++ src/components/submitInput/index.tsx
@@ -6,20 +6,20 @@ interface SubmitProps {
 export default function SubmitInput({ ...props }: SubmitProps): JSX.Element {
   return (
     <input 
-    type="submit" 
-    value={!props.value ? 'save' : props.value}
-    style={{
-      marginTop: '15px',
-      width: '100%',
-      padding: '8px',
-      borderRadius: '4px',
-      border: 'none',
-      outline: 'none',
-      backgroundColor: 'var(--contrast-std)',
-      color: 'var(--secondaire-l1)',
-      cursor: 'pointer',
-    }}
-    onClick={props.onClick}
+      type="submit" 
+      value={!props.value ? 'save' : props.value}
+      style={{
+        marginTop: '15px',
+        width: '100%',
+        padding: '8px',
+        borderRadius: '4px',
+        border: 'none',
+        outline: 'none',
+        backgroundColor: 'var(--contrast-std)',
+        color: 'var(--secondaire-l1)',
+        cursor: 'pointer',
+      }}
+      onClick={props.onClick}
     />
   )
 }
