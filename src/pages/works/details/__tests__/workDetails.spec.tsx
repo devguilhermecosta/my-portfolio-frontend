3ace693d6a88aecd07594e18e73c2d5a44115ba6
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-12-11 22:12:44 -0300

    refact

--- src/pages/works/details/__tests__/workDetails.spec.tsx
+++ src/pages/works/details/__tests__/workDetails.spec.tsx
@@ -1,10 +1,13 @@
 import '@testing-library/jest-dom';
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { server } from '../../../../utils/mocks/node';
 import { http, HttpResponse } from 'msw';
 import { baseUrl } from '../../../../utils/api';
 import WorkDetail from '..';
+
+const globalURL = global.URL.createObjectURL = vi.fn();
 
 describe('<WorkDetail />', () => {
   it('should return page not found if work not found', async () => {
@@ -38,5 +41,223 @@ describe('<WorkDetail />', () => {
     await screen.findByDisplayValue('work description');
     await screen.findByDisplayValue('https://my-work.com');
     await screen.findByAltText(/image of work title/i);
+  });
+
+  it('should change the field data', async () => {
+    // intercept the request
+    server.use(
+      http.get(`${baseUrl}/work/api/:slug/`, () => {
+        return HttpResponse.json({
+          id: 1,
+          title: 'work title',
+          description: 'work description',
+          link: 'https://my-work.com',
+          cover: 'work cover'
+        }, { status: 200 })
+      })
+    )
+
+    // new data to change input value
+    const newData = {
+      title: 'new title',
+      description: 'new description',
+      link: 'new link',
+      coverName: 'coverTest.png',
+    }
+
+    const user = userEvent.setup();
+
+    render(<WorkDetail />);
+
+    // get all fields
+    const title = await screen.findByDisplayValue('work title');
+    const description = await screen.findByDisplayValue('work description');
+    const link = await screen.findByDisplayValue('https://my-work.com');
+    await screen.findByAltText(/image of work title/i);  // cover
+
+    // clear all fields, except the cover
+    user.clear(title);
+    user.clear(description);
+    user.clear(link);
+
+    // type new field value
+    await userEvent.type(title, newData.title);
+    await userEvent.type(description, newData.description);
+    await userEvent.type(link, newData.link);
+
+    // mock the new cover
+    const inputUploadCover: HTMLInputElement = await screen.findByTestId('upload_images');
+    const newCover = new File(['text'], newData.coverName, { type: 'image/png' });
+    await userEvent.upload(inputUploadCover, newCover);
+
+    // checks if the new data is ok
+    expect(screen.getByDisplayValue(newData.title));
+    expect(screen.getByDisplayValue(newData.description));
+    expect(screen.getByDisplayValue(newData.link));
+    if (inputUploadCover.files) expect(inputUploadCover.files[0].name).toContain(newData.coverName);
+
+    // checks if the cover url has been created
+    expect(globalURL).toHaveBeenCalledTimes(1);
+
+  });
+
+  it('should return status code 400', async () => {
+    // intercept the request
+    server.use(
+      http.get(`${baseUrl}/work/api/:slug/`, () => {
+        return HttpResponse.json({
+          id: 1,
+          title: 'work title',
+          description: 'work description',
+          link: 'https://my-work.com',
+          cover: 'work cover'
+        }, { status: 200 })
+      }),
+      http.patch(`${baseUrl}/work/api/:slug/`, () => {
+        return new HttpResponse(null, { status: 400 })
+      })
+    )
+
+    const user = userEvent.setup();
+
+    render(<WorkDetail />);
+
+    const submitInput = await screen.findByDisplayValue(/save/);
+    const spy = vi.spyOn(console, 'error');
+    await user.click(submitInput);
+
+    expect(spy).toHaveBeenCalledWith('error on save with status 400');
+  });
+
+  it('should change the inputs to red when status code 400', async () => {
+    // intercept the request
+    server.use(
+      http.get(`${baseUrl}/work/api/:slug/`, () => {
+        return HttpResponse.json({
+          id: 1,
+          title: 'work title',
+          description: 'work description',
+          link: 'https://my-work.com',
+          cover: 'work cover'
+        }, { status: 200 })
+      }),
+      http.patch(`${baseUrl}/work/api/:slug/`, () => {
+        return HttpResponse.json({
+          link: 'Formato incorreto',
+          title: 'Campo obrigatório',
+          description: 'Campo obrigatório',
+        }, { status: 400 })
+      })
+    )
+
+    const user = userEvent.setup();
+    const borderError = '2px solid red';
+    render(<WorkDetail />);
+
+    const title = await screen.findByLabelText(/title/i);
+    const description = await screen.findByLabelText(/description/i);
+    const link = await screen.findByLabelText(/link/i);
+
+    // clear all fields, except the cover
+    user.clear(title);
+    user.clear(description);
+    user.clear(link);
+
+    // set the link with an invalid url
+    user.type(link, 'test');
+
+    const submitInput = await screen.findByDisplayValue(/save/);
+    await user.click(submitInput);
+
+    expect(title).toHaveStyle({ border: borderError });
+    expect(description).toHaveStyle({ border: borderError });
+    expect(link).toHaveStyle({ border: borderError });
+  });
+
+  it('should return status code 204', async () => {
+    // intercept the request
+    server.use(
+      http.get(`${baseUrl}/work/api/:slug/`, () => {
+        return HttpResponse.json({
+          id: 1,
+          title: 'work title',
+          description: 'work description',
+          link: 'https://my-work.com',
+          cover: 'work cover'
+        }, { status: 200 })
+      }),
+      http.patch(`${baseUrl}/work/api/:slug/`, () => {
+        return new HttpResponse(null, { status: 204 })
+      })
+    )
+
+    const user = userEvent.setup();
+    render(<WorkDetail />);
+
+    const submitInput = await screen.findByDisplayValue(/save/);
+    const spy = vi.spyOn(console, 'log');
+
+    await user.click(submitInput);
+
+    expect(spy).toHaveBeenCalledWith('save successfully with status code 204');
+  });
+
+  it('should show the changed value after patch request', async () => {
+    // intercept the request
+    server.use(
+      http.get(`${baseUrl}/work/api/:slug/`, () => {
+        return HttpResponse.json({
+          id: 1,
+          title: 'work title',
+          description: 'work description',
+          link: 'https://my-work.com',
+          cover: 'image of work title'
+        }, { status: 200 })
+      }),
+      http.patch(`${baseUrl}/work/api/:slug/`, () => {
+        return new HttpResponse(null, { status: 204 })
+      })
+    )
+
+    // new data to change input value
+    const newData = {
+      title: 'new title',
+      description: 'new description',
+      link: 'new link',
+      coverName: 'new cover',
+    }
+
+    const user = userEvent.setup();
+    render(<WorkDetail />);
+
+    // get all fields
+    const title = await screen.findByDisplayValue('work title');
+    const description = await screen.findByDisplayValue('work description');
+    const link = await screen.findByDisplayValue('https://my-work.com');
+    await screen.findByAltText(/image of work title/i);  // cover
+
+    // clear all fields, except the cover
+    user.clear(title);
+    user.clear(description);
+    user.clear(link);
+
+    // type new field value
+    await userEvent.type(title, newData.title);
+    await userEvent.type(description, newData.description);
+    await userEvent.type(link, newData.link);
+
+    // mock the new cover
+    const inputUploadCover: HTMLInputElement = await screen.findByTestId('upload_images');
+    const newCover = new File(['text'], newData.coverName, { type: 'image/png' });
+    await userEvent.upload(inputUploadCover, newCover);
+
+    const submitInput = await screen.findByDisplayValue(/save/);
+
+    await user.click(submitInput);
+
+    expect(await screen.findByDisplayValue(newData.title));
+    expect(await screen.findByDisplayValue(newData.description));
+    expect(await screen.findByDisplayValue(newData.link));
+    expect(await screen.findByAltText(`image of ${newData.title}`));
   });
 })
--- src/pages/works/details/index.tsx
+++ src/pages/works/details/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext, ChangeEvent, FormEvent } from 'react';
 import { useParams } from 'react-router-dom';
 import { api, baseUrl } from '../../../utils/api';
 import { WorkProps } from '../../../interfaces/work';
@@ -8,17 +8,27 @@ import Input from '../../../components/input';
 import TextArea from '../../../components/textarea';
 import Cover from '../../../components/cover';
 import Loading from '../../../components/loading';
+import UploadInput from '../../../components/uploadInput';
+import SubmitInput from '../../../components/submitInput';
+import { AuthContext } from '../../../contexts/authContext';
+import toast from 'react-hot-toast';
 
 export default function WorkDetail(): JSX.Element {
   const [work, setWork] = useState<WorkProps | undefined>();
   const [loading, setLoading] = useState(true);
 
-  const workId = work?.id;
   const { slug } = useParams();
+  const { user } = useContext(AuthContext);
 
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [link, setLink] = useState('');
+  const [cover, setCover] = useState<File | null>(null);
+  const [coverUrl, setCoverUrl] = useState<string | null>(null);
+
+  const [errorTitle, setErrorTitle] = useState('');
+  const [errorDescription, setErrorDescription] = useState('');
+  const [errorLink, setErrorLink] = useState('');
 
   useEffect(() => {
     async function getWork(slug: string | undefined): Promise<void> {
@@ -28,43 +38,132 @@ export default function WorkDetail(): JSX.Element {
         setTitle(response.data.title);
         setDescription(response.data.description);
         setLink(response.data?.link);
-        setLoading(false);
       })
       .catch(() => setWork(undefined))
+      .finally(() => setLoading(false))
     }
 
     getWork(slug);
 
   }, [slug])
 
+  function handleCover(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    const image = e.target.files;
+
+    if (image) {
+      setCover(image[0]);
+      setCoverUrl(URL.createObjectURL(image[0]));
+    }
+  }
+
+  if (loading) {
+    return (
+      <MainContainer>
+        <Loading />
+      </MainContainer>
+    )
+  }
+
+  function cleanErrorFields(): void {
+    setErrorTitle('');
+    setErrorDescription('');
+    setErrorLink('');
+  }
+
+  function addFieldToFormData(
+    formData: FormData, 
+    key: string, 
+    newValue: string, 
+    originalValue?: string): void {
+      if (originalValue !== newValue) formData.append(key, newValue);
+  }
+
+  async function handleSubmit(e: FormEvent<Element>): Promise<void> {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        'Content-Type': 'muiltipart/form-data',
+        'Authorization': `Bearer ${user}`,
+      }
+    }
+
+    const formData = new FormData();
+    addFieldToFormData(formData, 'title', title, work?.title);
+    addFieldToFormData(formData, 'description', description, work?.description);
+    addFieldToFormData(formData, 'link', link, work?.link)
+    if (cover) formData.append('cover', cover);
+
+    await api.patch(`${baseUrl}/work/api/${slug}/`, formData, config)
+    .then((r) => {
+      const msg = `save successfully with status code ${r.status}`;
+      console.log(msg);
+      toast.success(msg, { duration: 4000 });
+      cleanErrorFields();
+    })
+    .catch((e) => {
+      setErrorTitle(e.response.data?.title);
+      setErrorDescription(e.response.data?.description);
+      setErrorLink(e.response.data?.link ? e.response.data?.link[0] : '');
+      const msg = `error on save with status ${e.response.status}`
+      console.error(msg);
+      toast.error(msg, { duration: 4000 });
+    })
+  }
+
   return (
     <MainContainer>
 
-    {loading && <Loading />}
-
     {work 
-    ? <form style={{ width: '100%', maxWidth: '840px' }}>
-      <Input 
-        labelName='title' 
-        value={title} 
-        onChange={e => setTitle(e.target.value)}
-      />
+    ? <form
+      encType='multipart/form-data'
+        style={{ width: '100%', maxWidth: '840px' }}
+      >
+        <Input 
+          labelName='title' 
+          value={title}
+          error={errorTitle}
+          onChange={e => setTitle(e.target.value)}
+        />
 
-      <TextArea
-        label='description'
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
+        <TextArea
+          label='description'
+          value={description}
+          error={errorDescription}
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-      <Input 
-        labelName='link' 
-        value={link} 
-        onChange={e => setLink(e.target.value)}
-      />
+        <Input 
+          labelName='link' 
+          value={link}
+          error={errorLink}
+          onChange={e => setLink(e.target.value)}
+        />
 
-      {work?.cover && (
-        <Cover coverUrl={`${baseUrl}${work.cover}`} alt={`image of ${title}`}/>
-      )}
+        {work?.cover && (
+          <div style={{
+            position: 'relative',
+            width: 'fit-content',
+            margin: '0 auto',
+          }}>
+            <UploadInput 
+              position='absolute' 
+              top='50%' left='50%' 
+              transform='translate(-50%, -50%)'
+              multiple={false}
+              onChange={(e) => {handleCover(e)}}
+            />
+
+            {coverUrl 
+            ? <Cover coverUrl={coverUrl} alt={`image of ${cover?.name}`}/>
+            : <Cover coverUrl={`${baseUrl}${work.cover}`} alt={`image of ${title}`}/>
+            }
+            
+          </div>
+        )}
+
+        <SubmitInput onClick={(e) => handleSubmit(e)}/>
 
       </form>
     : <NotFound />
