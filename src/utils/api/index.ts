8ac3ca1973e0b5f36edf201fb574700ee5755e66
guilherme costa <guilherme.partic@gmail.com>
2023-12-25 22:22:42 -0300

    build

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
+const baseUrl = 'https://api.my-portfolio.devguilhermecosta.com';
 
 const api = axios.create({
   baseURL: baseUrl,
