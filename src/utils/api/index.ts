f8d5d0d663ccc18e7e1598060284125a00c865ed
Guilherme Costa <guilherme.partic@gmail.com>
2024-01-11 15:31:07 -0300

    update: add the semicolon

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api_key = import.meta.env.VITE_API_KEY
+const api_key = import.meta.env.VITE_API_KEY;
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
